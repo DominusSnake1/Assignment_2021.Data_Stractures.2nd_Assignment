6510be102e3aaf7a8045460a9421fc8e2573efdc
Dominus_Snake <77942136+DominusSnake1@users.noreply.github.com>
2021-12-29 03:39:01 +0200

    * Laid the Foundation.

---
+++ src/main/java/Deuteri_Ergasia/Dictionary.java
@@ -0,0 +1,28 @@
+package Deuteri_Ergasia;
+
+import java.util.Iterator;
+
+public interface Dictionary<K, V> extends Iterable<Dictionary.Entry<K, V>> {
+      void put(K key, V value);
+
+      V remove(K key);
+
+      V get(K key);
+
+      boolean contains(K key);
+
+      boolean isEmpty();
+
+      int size();
+
+      void clear();
+
+      Iterator<Entry<K, V>> iterator();
+
+      interface Entry<K, V> {
+            K getKey();
+
+            V getValue();
+      }
+}
+
